44840af15a325b1c4a514e37e8daa7e606c8fc2e
Dhinakar <dhinakar79044@gmail.com>
2025-07-09 23:08:55 +0530

    Initial commit

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,17 +1,37 @@
-import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-// Initialize Firebase Admin SDK
-function initFirebaseAdmin() {
-    const apps = getApps();
-
-    if (!apps.length) {
+const initFirebaseAdmin = () => {
+    if (!getApps().length) {
         initializeApp({
             credential: cert({
                 projectId: process.env.FIREBASE_PROJECT_ID,
                 clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                // Replace newlines in the private key
+                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // ✅ THIS IS NEEDED
+            }),
+        });
+    }
+
+    return {
+        auth: getAuth(),
+        db: getFirestore(),
+    };
+};
+
+export const { auth, db } = initFirebaseAdmin();
+
+/*
+import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const initFirebaseAdmin = () => {
+    if (!getApps().length) {
+        initializeApp({
+            credential: cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
                 privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
             }),
         });
@@ -21,6 +41,7 @@ function initFirebaseAdmin() {
         auth: getAuth(),
         db: getFirestore(),
     };
-}
+};
 
 export const { auth, db } = initFirebaseAdmin();
+*/
