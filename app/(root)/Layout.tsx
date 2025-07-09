452140611510dbbf229008bb36a9ded71bbc8b99
Dhinakar <dhinakar79044@gmail.com>
2025-07-09 23:23:22 +0530

    Initial commit

--- app/(root)/Layout.tsx
+++ app/(root)/Layout.tsx
@@ -1,3 +1,4 @@
+// app/layout.tsx or app/somepage/layout.tsx
 import Link from "next/link";
 import Image from "next/image";
 import { ReactNode } from "react";
@@ -7,47 +8,23 @@ import { isAuthenticated } from "@/lib/actions/auth.action";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
     const isUserAuthenticated = await isAuthenticated();
-    if (!isUserAuthenticated) redirect("/sign-in");
+
+    if (!isUserAuthenticated) {
+        redirect("/sign-in");
+    }
 
     return (
         <div className="root-layout">
-            <nav>
+            <nav className="flex items-center gap-2 p-4">
                 <Link href="/" className="flex items-center gap-2">
-                    <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-                    <h2 className="text-primary-100">PrepWise</h2>
+                    <Image src="/logo.svg" alt="PrepWise Logo" width={38} height={32} />
+                    <h2 className="text-primary-100 font-bold text-lg">PrepWise</h2>
                 </Link>
             </nav>
 
-            {children}
+            <main>{children}</main>
         </div>
     );
 };
 
 export default Layout;
-
-// import Link from "next/link";
-// import Image from "next/image";
-// import { ReactNode } from "react";
-// import { redirect } from "next/navigation";
-//
-// import { isAuthenticated } from "@/lib/actions/auth.action";
-//
-// const Layout = async ({ children }: { children: ReactNode }) => {
-//     const isUserAuthenticated = await isAuthenticated();
-//     if (!isUserAuthenticated) redirect("/sign-in");
-//
-//     return (
-//         <div className="root-layout">
-//             <nav>
-//                 <Link href="/" className="flex items-center gap-2">
-//                     <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-//                     <h2 className="text-primary-100">PrepWise</h2>
-//                 </Link>
-//             </nav>
-//
-//             {children}
-//         </div>
-//     );
-// };
-//
-// export default Layout;
